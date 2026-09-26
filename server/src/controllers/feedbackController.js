3309a33a289fb2de450d485d415e148867e3b5be
hana <nounihaytham@gmail.com>
2026-09-26 09:32:23 +0300

    feat: implement workshop feedback API

--- server/src/controllers/feedbackController.js
+++ server/src/controllers/feedbackController.js
@@ -4,7 +4,8 @@ import { Feedback } from '../models/Feedback.js';
 // TODO: implement per README.md section 2.
 export async function getAllFeedbacks(req, res, next) {
   try {
-    // TODO
+    const feedbacks = await Feedback.find();
+    res.status(200).json()[{feedbacks}];
   } catch (err) { next(err); }
 }
 
@@ -12,7 +13,14 @@ export async function getAllFeedbacks(req, res, next) {
 // TODO: implement per README.md section 2.
 export async function getFeedback(req, res, next) {
   try {
-    // TODO
+    const feedback = await Feedback.findById(req.params.id)
+    if (!feedback) {
+      return res.status(404).json({
+        message: 'Feedback not found'
+      });
+    }
+    res.status(200).json({feedback});
+
   } catch (err) { next(err); }
 }
 
@@ -20,7 +28,7 @@ export async function getFeedback(req, res, next) {
 // TODO: implement per README.md section 2.
 export async function createFeedback(req, res, next) {
   try {
-    // TODO
+     
   } catch (err) { next(err); }
 }
 
@@ -28,6 +36,43 @@ export async function createFeedback(req, res, next) {
 // TODO: implement per README.md section 3.
 export async function getFeedbackSummary(req, res, next) {
   try {
-    // TODO
-  } catch (err) { next(err); }
+    const { workshopCode } = req.query;
+
+    if (!workshopCode) {
+      return res.status(400).json({
+        message: 'workshopCode is required'
+      });
+    }
+
+    const result = await Feedback.aggregate([
+      {
+        $match: {
+          workshopCode: workshopCode
+        }
+      },
+      {
+        $group: {
+          _id: '$workshopCode',
+          averageScore: { $avg: '$score' },
+          feedbackCount: { $sum: 1 }
+        }
+      }
+    ]);
+
+    if (result.length === 0) {
+      return res.status(200).json({
+        workshopCode,
+        averageScore: 0,
+        feedbackCount: 0
+      });
+    }
+
+    res.status(200).json({
+      workshopCode,
+      averageScore: result[0].averageScore,
+      feedbackCount: result[0].feedbackCount
+    });
+  } catch (err) {
+    next(err);
+  }
 }
